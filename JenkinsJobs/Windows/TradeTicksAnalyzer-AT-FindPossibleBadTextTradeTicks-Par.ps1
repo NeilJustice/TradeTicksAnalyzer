4323c8917ed9472a756de1baa89e69bd4d3d7a8c
Neil Justice <njjustice@gmail.com>
2025-12-04 12:19:27 -0600

    Updates

---
+++ JenkinsJobs/Windows/TradeTicksAnalyzer-AT-FindPossibleBadTextTradeTicks-Par.ps1
@@ -0,0 +1,8 @@
+AcceptanceRunner.exe run-acceptance-test-with-multiple-possible-standard-outputs `
+   --acceptance-test-folder="AcceptanceTests\FindPossibleBadTextTradeTicks-Par" `
+   --program="C:\bin\TradeTicksAnalyzerRelease.exe" `
+   --program-args-file="ProgramArgs.txt" `
+   --required-standard-output-possibilities-file="RequiredStandardOutputPossibilities.txt" `
+   --required-text-file-writes-possibilities-folder="RequiredTextFileWrites" `
+   --test-runs=2 `
+   --required-exit-code=0
