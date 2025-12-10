94ec15cf8416aa7c3bf4ed569a9edaf5e0b0d146
Neil Justice <njjustice@gmail.com>
2025-12-09 18:50:58 -0600

    Updates

--- TradeTicksAnalyzer/TradeTicksAnalyzerMain.cpp
+++ TradeTicksAnalyzer/TradeTicksAnalyzerMain.cpp
@@ -7,8 +7,8 @@ using namespace std;
 
 // Production Args
 // ===============
-// find-possible-bad-text-trade-ticks --trading-logs-input-folder=X:\Trading\TradingProgram\PaperTradingLogsReplay --date=2025-12-08M --run-number=1 --bad-tick-change-percent-threshold=0.5 --trading-logs-output-folder=X:\Trading\TradingProgram\PaperTradingLogsReplay
-// find-possible-bad-text-trade-ticks --trading-logs-input-folder=X:\Trading\TradingProgram\PaperTradingLogsReplay --date=2025-12-08M --run-number=1 --bad-tick-change-percent-threshold=0.5 --trading-logs-output-folder=X:\Trading\TradingProgram\PaperTradingLogsReplay --parallel
+// find-possible-bad-text-trade-ticks --trading-logs-input-folder=X:\Trading\TradingProgram\PaperTradingLogsReplay --date=2025-12-09T --run-number=1 --bad-tick-change-percent-threshold=0.3 --trading-logs-output-folder=X:\Trading\TradingProgram\PaperTradingLogsReplay
+// find-possible-bad-text-trade-ticks --trading-logs-input-folder=X:\Trading\TradingProgram\PaperTradingLogsReplay --date=2025-12-09T --run-number=1 --bad-tick-change-percent-threshold=0.3 --trading-logs-output-folder=X:\Trading\TradingProgram\PaperTradingLogsReplay --parallel
 
 // Acceptance Tests
 // ================
